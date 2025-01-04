6fd07e894e24ef958ec9b3cafd36a93cd6391076
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-01-04 17:21:24 +0530

    chore: remove unnecessary test boilerplate

--- linklite/linklite/doctype/short_link/test_short_link.py
+++ linklite/linklite/doctype/short_link/test_short_link.py
@@ -2,29 +2,7 @@
 # See license.txt
 
 # import frappe
-from frappe.tests import IntegrationTestCase, UnitTestCase
-
-
-# On IntegrationTestCase, the doctype test records and all
-# link-field test record depdendencies are recursively loaded
-# Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-
+from frappe.tests import UnitTestCase
 
 class UnitTestShortLink(UnitTestCase):
-	"""
-	Unit tests for ShortLink.
-	Use this class for testing individual functions and methods.
-	"""
-
-	pass
-
-
-class IntegrationTestShortLink(IntegrationTestCase):
-	"""
-	Integration tests for ShortLink.
-	Use this class for testing interactions between multiple components.
-	"""
-
 	pass
