b51d3ccda14af3dbdcdc2ce1dcf50ae90881f9b0
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-03-30 12:10:56 +0530

    feat: copy to clipboard in desk

--- linklite/linklite/doctype/short_link/short_link.js
+++ linklite/linklite/doctype/short_link/short_link.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Build With Hussain and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Short Link", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Short Link", {
+	refresh(frm) {
+		frm.sidebar
+			.add_user_action(__("Copy short link"))
+			.attr("href", "#")
+			.on("click", () => {
+				const url = frappe.urllib.get_full_url(frm.doc.short_link);
+				frappe.utils.copy_to_clipboard(url, __("Short link copied"));
+			});
+	},
+});
