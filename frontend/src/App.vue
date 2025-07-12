854f80b8eb4dad261f99eba22744c49374b2598a
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-07-12 12:58:28 +0530

    refactor: new Layout component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <Toaster position="top-right" />
-  	<div class="flex h-screen w-full flex-row bg-surface-white shadow">
-		<Sidebar :header="{title: 'LinkLite', menuItems: [ { label: 'Toggle Theme', icon: Moon, onClick: toggleTheme },] }" />
-		<div class="w-full">
-			<router-view />
-		</div>
-	</div>
+	<Layout>
+		<router-view />
+	</Layout>
 </template>
 
 <script setup>
 import { Toaster } from 'vue-sonner';
-import Moon from '~icons/lucide/moon';
-import { Sidebar } from 'frappe-ui';
-
-function toggleTheme() {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  document.documentElement.setAttribute('data-theme', newTheme);
-}
+import Layout from './Layout.vue';
 </script>
---
+++ frontend/src/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="flex h-screen w-full flex-row bg-surface-white shadow">
+		<Sidebar :header="{title: 'LinkLite', menuItems: [ { label: 'Toggle Theme', icon: Moon, onClick: toggleTheme },] }" />
+		<div class="w-full">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import Moon from '~icons/lucide/moon';
+import { Sidebar } from 'frappe-ui';
+
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
+}
+</script>
